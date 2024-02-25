5698439f40647596866f96f00be52fff28724f83
mndgs1 <mindaugasbankauskas@gmail.com>
2024-02-25 15:02:55 +0100

    Added datepicker hover effect

--- src/components/common/DatePicker/DatePicker.tsx
+++ src/components/common/DatePicker/DatePicker.tsx
@@ -23,12 +23,12 @@ const DatePicker = ({ value, onChange, disabledDays }: any) => {
             onChange={onChange}
             shouldHighlightWeekends
             inputPlaceholder="Check Availability"
-            inputClassName={`w-full h-13 rounded-lg cursor-pointer border border-primary px-3.5 text-center py-2.5 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent ${
+            inputClassName={`w-full h-13 rounded-lg cursor-pointer hover:ring-2 hover:ring-primary hover:ring-rounded hover:border-transparent border border-primary px-3.5 text-center py-2.5 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent ${
                 value.from && value.to
                     ? "ring-2 ring-primary border-transparent"
                     : ""
             }`}
-            wrapperClassName="w-full mb-2.5 z-10 cursor-pointer"
+            wrapperClassName="w-full mb-2.5 z-10 cursor-pointer "
             colorPrimaryLight="#D1FAE5"
             colorPrimary="#10B981"
             minimumDate={minimumDate}
